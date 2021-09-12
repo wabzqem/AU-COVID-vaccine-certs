c2bc1533be61db69b5eb326d5592ed42c2f9ac11
Richard Nelson <wabz@whatsbeef.net>
2021-09-12 12:56:07 +1000

    Initial Commit

---
+++ Shared/AU_COVID_VerifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  AU_COVID_VerifierApp.swift
+//  Shared
+//
+//  Created by Richard Nelson on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AU_COVID_VerifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
